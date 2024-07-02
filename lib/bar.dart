4587362ab4f5b039577849aa0b7ea147f91f4ac9
Aziza Tahsin Nawshin <32642730+atnawshin@users.noreply.github.com>
2024-07-03 01:21:16 +0600

    Bar Chart updated

--- lib/bar.dart
+++ lib/bar.dart
@@ -16,108 +16,132 @@ class _barState extends State<bar> {
         child: AspectRatio(
           aspectRatio: 2.0,
           child: Container(
-            margin: EdgeInsets.all(24),
+            margin: const EdgeInsets.all(24),
             // color: Colors.red,
             child: BarChart(
               BarChartData(
-                barGroups: [
-                  BarChartGroupData(x: 0, barRods: [
-                    BarChartRodData(toY: 4),
-                    BarChartRodData(toY: 6)
-                  ]),
-                  BarChartGroupData(x: 1, barRods: [
-                    BarChartRodData(toY: 4),
-                    BarChartRodData(toY: 6)
-                  ]),
-                  BarChartGroupData(x: 2, barRods: [
-                    BarChartRodData(toY: 14),
-                    BarChartRodData(toY: 10)
-                  ]),
-                  BarChartGroupData(x: 3, barRods: [
-                    BarChartRodData(toY: 16),
-                    BarChartRodData(toY: 7)
-                  ]),
-                  // BarChartGroupData(x: 0, barRods: [
-                  //   BarChartRodData(toY: 7),
-                  //   BarChartRodData(toY: 11)
-                  // ]),
-                  // BarChartGroupData(x: 0, barRods: [
-                  //   BarChartRodData(toY: 6),
-                  //   BarChartRodData(toY: 3)
-                  // ]),
-                  // BarChartGroupData(x: 0, barRods: [
-                  //   BarChartRodData(toY: 8),
-                  //   BarChartRodData(toY: 14)
-                  // ]),
-                ],
-                alignment: BarChartAlignment.start,
-                groupsSpace: 10,
-                titlesData: FlTitlesData(
-                  show: true,
-                  leftTitles: AxisTitles(
-                    // axisNameWidget: Text("Y-axis"),
-                    axisNameWidget: Icon(Icons.access_alarm),
-                    axisNameSize: 10,
-                    // drawBelowEverything: false,
-                    sideTitles: SideTitles(
-                      reservedSize: 40,
-                      showTitles: true,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        // return Align(
-                        //   alignment: Alignment.centerRight,
-                        //   child: Padding(
-                        //     padding: const EdgeInsets.only(right: 8.0),
-                        //     child: Text(
-                        //       meta.formattedValue,
-                        //       style: TextStyle(
-                        //         color: Colors.blue,
-                        //         fontWeight: FontWeight.bold,
-                        //       ),
-                        //     ),
-                        //   ),
-                        // );
-                        return SideTitleWidget(
-                          axisSide: meta.axisSide,
-                          child: switch (value) {
-                            0 => Text(meta.formattedValue),
-                            5 => TextButton(
-                                onPressed: () {},
-                                child: Text(meta.formattedValue),
-                              ),
-                            _ => const Icon(Icons.account_balance),
-                          },
-                        );
-                        //   return SideTitleWidget(
-                        //       axisSide: meta.axisSide,
-                        //       //does the same thing as above
-                        //       child:
-                        //           //  Text(
-                        //           //   meta.formattedValue,
-                        //           //   style: const TextStyle(
-                        //           //     color: Colors.blue,
-                        //           //     fontWeight: FontWeight.bold,
-                        //           //   ),
-                        //           // ),
-                        //           //     Icon(
-                        //           //   Icons.add,
-                        //           //   size: 14,
-                        //           // ),
-                        //           TextButton(
-                        //         onPressed: () {},
-                        //         child: Text(meta.formattedValue),
-                        //       ));
-                      },
-                    ),
-                  ),
-                  // bottomTitles: AxisTitles(
-                  //   axisNameWidget: Text("X-axis"),
-                  //   sideTitles: SideTitles(
-                  //     reservedSize: 44,
-                  //     showTitles: true,
-                  //   ),
+                  barGroups: [
+                    BarChartGroupData(x: 0, barRods: [
+                      BarChartRodData(toY: 4),
+                      BarChartRodData(toY: 6)
+                    ]),
+                    BarChartGroupData(x: 1, barRods: [
+                      BarChartRodData(toY: 4),
+                      BarChartRodData(toY: 6)
+                    ]),
+                    BarChartGroupData(x: 2, barRods: [
+                      BarChartRodData(toY: 14),
+                      BarChartRodData(toY: 10)
+                    ]),
+                    BarChartGroupData(x: 3, barRods: [
+                      BarChartRodData(toY: 15),
+                      BarChartRodData(toY: 7)
+                    ]),
+                    // BarChartGroupData(x: 0, barRods: [
+                    //   BarChartRodData(toY: 7),
+                    //   BarChartRodData(toY: 11)
+                    // ]),
+                    // BarChartGroupData(x: 0, barRods: [
+                    //   BarChartRodData(toY: 6),
+                    //   BarChartRodData(toY: 3)
+                    // ]),
+                    // BarChartGroupData(x: 0, barRods: [
+                    //   BarChartRodData(toY: 8),
+                    //   BarChartRodData(toY: 14)
+                    // ]),
+                  ],
+                  alignment: BarChartAlignment.start,
+                  groupsSpace: 10,
+                  backgroundColor: Colors.purple.withOpacity(0.15)
+                  // rangeAnnotations: RangeAnnotations(
+                  //   horizontalRangeAnnotations: [
+                  //     HorizontalRangeAnnotation(
+                  //       y1: 4,
+                  //       y2: 8,
+                  //       color: Colors.red.withOpacity(0.5),
+                  //     ),
+                  //     HorizontalRangeAnnotation(
+                  //       y1: 10,
+                  //       y2: 12,
+                  //       // color: Colors.red.withOpacity(0.5),
+                  //       gradient: const LinearGradient(
+                  //         colors: [
+                  //           Colors.red,
+                  //           Colors.green,
+                  //           Colors.purple,
+                  //         ],
+                  //       ),
+                  //     ),
+                  //   ],
                   // ),
-                ),
-              ),
+                  // titlesData: const FlTitlesData(
+                  //   show: true,
+                  //   leftTitles: AxisTitles(
+                  //     // axisNameWidget: Text("Y-axis"),
+                  //     axisNameWidget: Icon(Icons.access_alarm),
+                  //     axisNameSize: 10,
+                  //     // drawBelowEverything: false,
+                  //     sideTitles: SideTitles(
+                  //       reservedSize: 40,
+                  //       showTitles: true,
+                  //       // interval: 4,
+                  //       // interval: null,
+                  //       // getTitlesWidget: (double value, TitleMeta meta) {
+                  //       // return Align(
+                  //       //   alignment: Alignment.centerRight,
+                  //       //   child: Padding(
+                  //       //     padding: const EdgeInsets.only(right: 8.0),
+                  //       //     child: Text(
+                  //       //       meta.formattedValue,
+                  //       //       style: TextStyle(
+                  //       //         color: Colors.blue,
+                  //       //         fontWeight: FontWeight.bold,
+                  //       //       ),
+                  //       //     ),
+                  //       //   ),
+                  //       // );
+                  //       // return SideTitleWidget(
+                  //       //   axisSide: meta.axisSide,
+                  //       //   child: switch (value) {
+                  //       //     0 => Text(meta.formattedValue),
+                  //       //     5 => TextButton(
+                  //       //         onPressed: () {},
+                  //       //         child: Text(meta.formattedValue),
+                  //       //       ),
+                  //       //     _ => const Icon(Icons.account_balance),
+                  //       //   },
+                  //       // );
+                  //       //   return SideTitleWidget(
+                  //       //       axisSide: meta.axisSide,
+                  //       //       //does the same thing as above
+                  //       //       child:
+                  //       //           //  Text(
+                  //       //           //   meta.formattedValue,
+                  //       //           //   style: const TextStyle(
+                  //       //           //     color: Colors.blue,
+                  //       //           //     fontWeight: FontWeight.bold,
+                  //       //           //   ),
+                  //       //           // ),
+                  //       //           //     Icon(
+                  //       //           //   Icons.add,
+                  //       //           //   size: 14,
+                  //       //           // ),
+                  //       //           TextButton(
+                  //       //         onPressed: () {},
+                  //       //         child: Text(meta.formattedValue),
+                  //       //       ));
+                  //       // },
+                  //     ),
+                  //   ),
+                  //   // bottomTitles: AxisTitles(
+                  //   //   axisNameWidget: Text("X-axis"),
+                  //   //   sideTitles: SideTitles(
+                  //   //     reservedSize: 44,
+                  //   //     showTitles: true,
+                  //   //   ),
+                  //   // ),
+                  // ),
+                  ),
             ),
           ),
         ),
